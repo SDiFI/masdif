f70a27335c8b00120022814cf900df39eaae0942
Daniel Schnell <dschnell@grammatek.com>
2023-03-20 23:25:37 +0000

    Add CORS configuration with wildcard origin policy.
    Add CorsController with route OPTIONS /
    
    To debug, use e.g. the following command to trigger your CORS tests:
    
    curl -X OPTIONS -H "Origin: http://sdifi-project.com" -i http://localhost:3000/
    
    Signed-off-by: Daniel Schnell <dschnell@grammatek.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :conversations
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   # defines the route for the health check
   get '/health', to: 'health#index'
+
+  # defines the route for the cors options
+  match '/', to: 'cors#options', via: :options
 end
