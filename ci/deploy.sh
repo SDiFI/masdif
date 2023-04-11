5ac13cecb02f85f30fdc96e33006495dc2530e35
Daniel Schnell <dschnell@grammatek.com>
2023-04-11 15:18:25 +0000

    Final deployment preparations for Masdif v0.1
    - README.md: add explanations for Rasa models and caching model dependencies
        update supported Endpoints, update Throughput section
    - docker-compose.yml: add env_file entry, otherwise the environment is not
      set for each service, use default network instead of 'backend',
      move action_server to rasa-project
    - start-masdif.sh: fix test for cloned repo, fix build step
    - update Rasa version to 3.4.9
    - build.yml: copy example environment to .env, build complete images including
      those from the municipal rasa demo
    - download Rasa model from specific municipality release version
    - add deploy.sh script: Upload docker-compose scripts, Rasa model and configuration.
      Create all necessary directories, run Rails migrations
    - depoy_if_tag.yml: build container on a pushed tag and tag it + push to
      container registgry.
    - Configure ssh on runner
    
    Signed-off-by: Daniel Schnell <dschnell@grammatek.com>

---
+++ ci/deploy.sh
@@ -0,0 +1,71 @@
+# Deploy the application to a remote server
+#
+# Usaage: deploy.sh tag user@host
+#   The tag is the git tag to deploy
+#   The user@host is the user and host to deploy to
+#
+# Prerequisites:
+#   - The user@host must have access to the private container registry (and be logged in)
+#   - The images have to be built beforehand with the given tag
+#   - RAILS_MASTER_KEY must be set in the environment
+
+DEPLOY_DIR=masdif
+ENV_FILE=.env.deploy
+MASTER_KEY=$RAILS_MASTER_KEY
+USER_HOST=$2
+TAG=$1
+
+set -eo pipefail
+
+if [ -z "$TAG" ]; then
+    echo "tag is missing"
+    echo "Usage: deploy.sh tag user@host"
+    exit 1
+fi
+if [ -z "$USER_HOST" ]; then
+    echo "user@host is missing"
+    echo "Usage: deploy.sh tag user@host"
+    exit 1
+fi
+
+if [ -z "$MASTER_KEY" ]; then
+    echo "variable RAILS_MASTER_KEY is not set"
+    exit 1
+fi
+
+# Prepare .env file on server
+cp .env.example $ENV_FILE
+echo "APPLICATION_VERSION=$TAG" >> $ENV_FILE
+echo "RAILS_MASTER_KEY=$MASTER_KEY" >> $ENV_FILE
+
+# Create directory $DEPLOY_DIR with subdirectories on remote server
+# Copy the following files into $DEPLOY_DIR on remote server :
+#   - docker-compose.yml
+#   - masdif_override_template.yml
+#   - $ENV_FILE
+#   - Rasa configuration files and Rasa model
+ssh "$USER_HOST" "mkdir -p $DEPLOY_DIR"
+ssh "$USER_HOST" "mkdir -p $DEPLOY_DIR/rasa/models"
+ssh "$USER_HOST" "mkdir -p $DEPLOY_DIR/rasa/cache"
+ssh "$USER_HOST" "mkdir -p $DEPLOY_DIR/config/rasa"
+
+scp config/rasa/endpoints.yml "$USER_HOST":$DEPLOY_DIR/config/rasa
+scp config/rasa/credentials.yml "$USER_HOST":$DEPLOY_DIR/config/rasa
+scp rasa/config.yml "$USER_HOST":$DEPLOY_DIR/rasa
+scp docker-compose.yml "$USER_HOST":$DEPLOY_DIR
+scp rasa/masdif_override_template.yml "$USER_HOST":$DEPLOY_DIR/rasa
+scp $ENV_FILE "$USER_HOST":$DEPLOY_DIR/.env
+scp rasa/models/* "$USER_HOST":$DEPLOY_DIR/rasa/models
+
+# Pull all Masdif images from container registry, use the tag from the environment variable APPLICATION_VERSION
+# Note: we use docker compose here instead of docker-compose !
+ssh "$USER_HOST" "cd $DEPLOY_DIR && cat docker-compose.yml rasa/masdif_override_template.yml | docker compose -f - pull"
+
+# Stop eventually running Masdif containers
+ssh "$USER_HOST" "cd $DEPLOY_DIR && cat docker-compose.yml rasa/masdif_override_template.yml | docker compose -f - down"
+
+# Start Masdif containers
+ssh "$USER_HOST" "cd $DEPLOY_DIR && cat docker-compose.yml rasa/masdif_override_template.yml | docker compose -f - up -d --no-build"
+
+# run rails migration on server
+ssh "$USER_HOST" "cd $DEPLOY_DIR && docker compose exec -it masdif bin/rails db:prepare"
