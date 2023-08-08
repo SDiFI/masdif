55fe854543d0401de8ea894287ae0a106dacff95
Daniel Schnell <dschnell@grammatek.com>
2023-08-08 15:46:07 +0000

    Refactor out ActiveAdmin#Message table rendering definitions
    This extracts the Message table rendering so that we can reuse the exact
    same rendering code inside the Message and the Conversation#Message Panel
    drill-down view.
    
    Adapt the SCSS code to apply also for the conversation_message_table class.
    
    This addresses issues #42, #43
    
    Signed-off-by: Daniel Schnell <dschnell@grammatek.com>

---
+++ app/admin/shared_message_defs.rb
@@ -0,0 +1,39 @@
+module SharedMessageDefs
+  extend ActiveSupport::Concern
+
+  # Define the message columns of a messages table
+  def message_columns
+    column 'User text' do |m|
+      link_to m.text, admin_message_path(m, scope: params[:scope])
+    end
+    column 'Bot answer', :reply_text
+    # CSS highlighting rules apply to 'col-user_feedback' column
+    column 'User Feedback', :feedback, class: 'col-user_feedback'
+    column 'Intent', :intent
+    column 'Entities', :entities
+    column 'Slots', :slots
+    column 'Actions', :actions
+    column 'Voice Audio' do |m|
+      audio_urls = m.audio_urls
+      if audio_urls.nil? || audio_urls.empty?
+        'N/A'
+      else
+        audio_tag audio_urls, controls: true, preload: 'none'
+      end
+    end
+  end
+
+  # Return the CSS row_class to highlight the feedback
+  #
+  # @param [Message] msg the message
+  # @return [String] the CSS class
+  def highlight_feedback(msg)
+    if msg.feedback == 'positive'
+      'highlight-positive'
+    elsif msg.feedback == 'negative'
+      'highlight-negative'
+    else
+      'highlight-none'
+    end
+  end
+end
