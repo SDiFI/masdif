7d90784427b71017e069de11f425a772c7bbecf5
Daniel Schnell <dschnell@grammatek.com>
2023-08-18 10:07:01 +0000

    DB migrations: make the seed data idempotent
    Make the DB seed data idempotent, i.e. don't rely on previously applied
    data from a migration.
    
    Explanation:
    A call to rails db:prepare will not actually run the migrations but instead
    uses rake db:schema_load directly, so that none of the migrations are
    run. In the case that data migrations are done via normal migrations,
    these are left out via db;prepare as well. If one wants to make sure, that
    all migrations are run, one needs to explicitly call
    
      rake db:migrate db:seed.
    
    - move the data migration for roles out of the migration itself
    into seeds.rb.
    
    
    Signed-off-by: Daniel Schnell <dschnell@grammatek.com>

--- db/migrate/20230811132259_create_roles.rb
+++ db/migrate/20230811132259_create_roles.rb
@@ -1,14 +1,8 @@
 class CreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table :roles, id: :uuid do |t|
-      t.string :name
-
+      t.string :name, null: false
       t.timestamps
-    end
-
-    # prepopulate roles
-    %w[user admin].each do |role|
-      Role.create(name: role)
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Seed the database with initial data
 
-# Roles have already been created inside the migration itself
+# prepopulate roles
+%w[user admin].each do |role|
+  Role.create!(name: role)
+end
+
+admin_role = Role.find_by(name: :'admin')
+if admin_role.nil?
+  raise "Could not find admin role ?!"
+end
 
 # Create a dummy default admin user for the admin interface in development
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: :admin) if Rails.env.development?
-
-# For production, we want to make sure that we never create a default admin user. Therefore it needs to be explicitly
-# created via environment variables or Rails credentials.
-if Rails.env.production?
+if Rails.env.development?
+  User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: admin_role)
+elsif Rails.env.production?
+  # For production, we want to make sure that we never create a default admin user. Therefore it needs to be explicitly
+  # created via environment variables or Rails credentials. You can change this later in the admin interface.
   admin_user = Rails.application.credentials&.admin_user || ENV['ADMIN_USER']
   admin_password = Rails.application.credentials&.admin_password || ENV['ADMIN_PASSWORD']
   if admin_user && admin_password
-    User.create!(email: admin_user, password: admin_password, password_confirmation: admin_password, role: :admin)
+    User.create!(email: admin_user, password: admin_password, password_confirmation: admin_password, role: admin_role)
   else
     raise "Missing ADMIN_USER and ADMIN_PASSWORD environment variables, or Rails credentials."
   end
